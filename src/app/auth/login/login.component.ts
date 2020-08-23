f08dc25fa10b9dfd61f4fbdb366da2168ce94d70
fukanoko <fukanoko0312@gmail.com>
2020-08-23 12:00:04 +0900

    Auth 修正

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -21,6 +21,7 @@ export class LoginComponent implements OnInit {
     login(loginForm){
         this.authService.login(loginForm.value).subscribe(
             (token) => {
+                console.log(token)
                 this.router.navigate(['/products'])
             },
             (err: HttpErrorResponse) => {
